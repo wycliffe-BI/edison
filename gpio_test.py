61900bfcfff409ce554d57d7680b2b90480f87ad
brendan <indb@wycliffe.co.uk>
2020-07-06 16:11:28 +0100

    syntax changes

--- gpio_test.py
+++ gpio_test.py
@@ -1,26 +1,25 @@
-#GPIO test for the edison, simple code for turing digital pins on and off.
+# GPIO test for the edison, simple code for turing digital pins on and off.
 
-#To interface with digital/analog pins on the edison, we use a library called MRAA which can act as the equivelant of analogRead(pin), as if we are programming
-#with the Arduino IDE.
+# To interface with digital/analog pins on the edison, we use a library called MRAA which can act as the equivelant of analogRead(pin), as if we are programming
+# with the Arduino IDE.
 
-#!/usr/bin/python
+# !/usr/bin/python
 
 print("Hello world")
 
-import mraa     # For accessing the GPIO
-from time import sleep     # For sleeping between blinks
+import mraa  # For accessing the GPIO
+from time import sleep  # For sleeping between blinks
 
 led_pin = 5  # we are using D5 pin
 
-
 blinkLed = mraa.Gpio(led_pin)  # We make an object, which is the led that we will blink.
 
-
-blinkLed.dir(mraa.DIR_OUT)  # Set the direction as output (i.e) this object has a property where we allocate the INPUT or OUTPUT
+blinkLed.dir(
+    mraa.DIR_OUT)  # Set the direction as output (i.e) this object has a property where we allocate the INPUT or OUTPUT
 
 ledState = False  # LED is off to begin with
 
-blinkLed.write(0) #Write the blinking led object to low.
+blinkLed.write(0)  # Write the blinking led object to low.
 
 # One infinite loop coming up
 while True:
